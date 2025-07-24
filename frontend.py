3beb84e3c7273314083c01cd14ae1861a1b3e508
User <osayamenedoboyi@gmail.com>
2025-07-24 10:16:52 +0100

    yes

--- frontend.py
+++ frontend.py
@@ -1,1 +1,7 @@
 import streamlit as st
+import numpy
+st.title("""Welcome To A Football Player Rating App
+insert your username to sign in """)
+inputs = st.text_input("Please enter a user_name here")
+st.write("your username is",inputs)
+
